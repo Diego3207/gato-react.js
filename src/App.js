438c9ac115277381bd1b3f35c20afde4ce74a022
Diego3207 <brizuelamer369.damb@gmail.com>
2021-04-28 16:40:04 -0500

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import { useState } from 'react';
 import './App.css';
 import Board from './components/Board/Board';
 
+const winningPositions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const App = () => {
   const [turn, setTurn]  = useState('X');
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [winingSquares, setWiningSquares] = useState([]);
   const [score, setScore] = useState({
     X: 0,
     O: 0,
   });
 
-  const checkForWinner = squares => {
+  const checkForWinner = newSquares => {
+    for(let i = 0; i < winningPositions.length; i++) {
+      const [a,b,c] = winningPositions[i];
+      if(newSquares[a] && newSquares[a] === newSquares[b] && newSquares[a] === newSquares [c]){
+        // hay un ganador
+        endGame(newSquares[a], winningPositions[i]);
+        return
+      }
+    }
+
+    if(!newSquares.includes(null)) {
+      //hay un empate
+      endGame(null, Array.from(Array(10).keys()));
+      return
+    }
+
     setTurn(turn === 'X' ? 'O' : 'X');
   }
 
@@ -19,6 +46,18 @@ const App = () => {
     newSquares.splice(square, 1, turn);
     setSquares(newSquares);
     checkForWinner(newSquares);
+  }
+
+  const endGame = (result, winningPositions) => {
+    setTurn(null);
+    if(result !== null) {
+      setScore({
+        ...score,
+        [result]: score[result] + 1,
+      })
+    }
+
+
   }
 
   return (
